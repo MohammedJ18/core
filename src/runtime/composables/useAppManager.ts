d657ac57adee7671ce130df9afe9b7faf8d74259
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-11 21:30:39 +0300

    qwd

--- src/runtime/composables/useAppManager.ts
+++ src/runtime/composables/useAppManager.ts
@@ -14,15 +14,13 @@ export const useAppManager = defineStore("app-manager", {
         getApp: state => id => state.apps.find(app => app.id === id),
         getRunningApps: (state) => state.apps.filter(app => app.running),
         getOwned: (state) => state.apps.filter(app => app.owned),
-        // getLockedApps: (state) => state.all.filter(a => a.locked),
-        getFocused: (state) => state.focused,
-        getChildrenApps: (state) => (title) => state.apps.filter(app => app.parentApp === title),
+        getFocused: (state) => state.apps.find(app => app.id == state.focused),
         anyRunningIsMaximized: (state) => state.apps.some(app => app.maximized && app.running && !app.minimized),
     },
 
     actions: {
-        setFocus(title){
-            this.focused = title;
+        setFocus(id){
+            this.focused = id;
         },
         addApp(app){
             this.apps.push(new App({ id:this.apps.length+1, ...app}));
